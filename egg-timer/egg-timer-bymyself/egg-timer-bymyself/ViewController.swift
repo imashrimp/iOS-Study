0b93ca81bd718b38abe11fc7c8f6f69d09cbcc45
kwonhs8287 <kwonhs8287@gmail.com>
2022-12-06 14:32:20 +0900

    function-done

--- egg-timer/egg-timer-bymyself/egg-timer-bymyself/ViewController.swift
+++ egg-timer/egg-timer-bymyself/egg-timer-bymyself/ViewController.swift
@@ -10,32 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    /*let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12*/
-    
     let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        let hardness = sender.currentTitle
         
-        /*switch hardness  {
-        case "Soft":
-            print(softTime)
-        case "Medium":
-            print(mediumTime)
-        default:
-            print(hardTime)
-        }*/ //switch statement 사용 시
+        let hardness = sender.currentTitle!
         
-        if sender.currentTitle == "Soft"{
-            print("Soft")
-        }else if sender.currentTitle == "Medium" {
-            print("Medium")
-        }else {
-            print("Hard")
-        }
-      
+        let result = eggTimes[hardness]!
         
+        print(result)
     }
 }
