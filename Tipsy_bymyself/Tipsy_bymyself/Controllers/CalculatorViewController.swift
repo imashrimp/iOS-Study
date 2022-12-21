7a44e1266921af124cfc12e76d7bb204721a6c0b
kwonhs8287 <kwonhs8287@gmail.com>
2022-12-21 16:35:04 +0900

    complete Tipsy_bymyself

--- Tipsy_bymyself/Tipsy_bymyself/Controllers/CalculatorViewController.swift
+++ Tipsy_bymyself/Tipsy_bymyself/Controllers/CalculatorViewController.swift
@@ -15,34 +15,66 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
     
-    var buttonSelected: Float = 0.0
+    var tip = 0.10
+    var numberOfPeople = 2
+    var billTotal = 0.0
+    var finalResult = "0.0"
     
     
     @IBAction func tipChanged(_ sender: UIButton) {
+        //        tipChanged 탭 했을 때 숫자 키패드 사라지게하는 코드
+        billTextField.endEditing(true)
+        
         //        아래 네 줄이 버튼 눌렀을 때 그 버튼의 바탕에 불 들어오는 코드
         zeroPctButton.isSelected = false
         tenPctButton.isSelected = false
         twentyPctButton.isSelected = false
         sender.isSelected = true
         
-        var buttonPressed = sender.currentTitle
-        if buttonPressed == "0%" {
-            buttonSelected = 0.0
-        }else if buttonPressed == "10%" {
-            buttonSelected = 0.1
-        }else if buttonPressed == "20%" {
-            buttonSelected = 0.2
-        }
+        let buttonTitle = sender.currentTitle!
+        let buttonTitleMinusPerscentSign = String(buttonTitle.dropLast())
+        let buttonTitleAsANumber = Double(buttonTitleMinusPerscentSign)!
+        tip = buttonTitleAsANumber / 100
+        
+        /*
+         let buttonPressed = sender.currentTitle
+         if buttonPressed == "0%" {
+         tip = 0.0
+         }else if buttonPressed == "10%" {
+         tip = 0.1
+         }else if buttonPressed == "20%" {
+         tip = 0.2
+         }
+         */
+        
+        
         
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
         splitNumberLabel.text = String(format: "%.0f", sender.value)
+        numberOfPeople = Int(sender.value)
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
-        print(buttonSelected)
-        print(splitNumberLabel.text!)
+        
+        let bill = billTextField.text!
+        if bill != "" {
+            billTotal = Double(bill)!
+            let result = billTotal * (1 + tip) / Double(numberOfPeople)
+            finalResult = String(format: "%.2f", result)
+        }
+        performSegue(withIdentifier: "goToResult", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "goToResult" {
+            let destinationVC = segue.destination as! ResultViewController
+            destinationVC.result = finalResult
+            destinationVC.tip = Int(tip * 100)
+            destinationVC.split = numberOfPeople
+        }
     }
 }
 
