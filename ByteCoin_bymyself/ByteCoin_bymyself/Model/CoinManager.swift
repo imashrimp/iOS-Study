f43462fca5270d76e4dfa06e8c730517bfb46697
kwonhs8287 <kwonhs8287@gmail.com>
2023-01-10 21:08:15 +0900

    networking

--- ByteCoin_bymyself/ByteCoin_bymyself/Model/CoinManager.swift
+++ ByteCoin_bymyself/ByteCoin_bymyself/Model/CoinManager.swift
@@ -15,7 +15,7 @@ struct CoinManager {
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
     
     
-//    clima와 다르게 여기서는 fetch메서드를 따로 사용하지 않는 이유가 있는가?
+//    clima와 다르게 여기서는 fetch메서드를 따로 사용하지 않는 이유가 있는가? => api를 통해 data를 받아와 실행되는 함수에 networking이 필요하다. 현재 step3에서는 getCoinPrice 메서드에서만 api networking을 통해 받은 data를 사용하므로 여기에 networking과정이 사용된다.
     func getCoinPrice(for currency: String) {
         let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
         
